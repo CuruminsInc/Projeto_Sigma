3ac6435903ec3623b6f328785e5293db44f4f6a3
José Luiz Rodrigues dos Santos <xgjlrs@gmail.com>
2024-11-30 00:17:56 -0400

    History screens completed

--- app/history/history.vm.tsx
+++ app/history/history.vm.tsx
@@ -22,6 +22,161 @@ const useHistory = () => {
     const [model, setModel] = useState('');
     const [year, setYear] = useState('');
     const [accessories, setAccessories] = useState('');
+    const [openItemId, setOpenItemId] = useState<string | null>(null);
+
+    const handlePress = (id: string) => {
+        setOpenItemId(prevId => (prevId === id ? null : id));
+    };
+    
+    const dataBuy = [
+        {
+            id: 1,
+            client: 'Ayrton Sandro de Bragança',
+            seller: 'José Almeida Fidalgo Lapazoli',
+            date: '18/03/2016',
+            vehicle: 'Carro X',
+            brand: 'Marca Y',
+            model: 'Modelo Z',
+            color: 'Preto',
+            totalvalue: 10000
+        },
+        {
+            id: 2,
+            client: 'Mario Lapazoli de Lins',
+            seller: 'José Luiz Rodrigues dos Santos',
+            date: '24/02/2016',
+            vehicle: 'Carro A',
+            brand: 'Marca Z',
+            model: 'Modelo Y',
+            color: 'Vermelho',
+            totalvalue: 40000
+        },
+        {
+            id: 3,
+            client: 'João Vitor Benchimol',
+            seller: 'José Almeida Fidalgo Lapazoli',
+            date: '05/02/2016',
+            vehicle: 'Carro F',
+            brand: 'Marca R',
+            model: 'Modelo V',
+            color: 'Azul',
+            totalvalue: 1000
+        },
+        {
+            id: 4,
+            client: 'Ayrton Senna Bate Cuca',
+            seller: 'Marcos Adriano de Bragança Lins',
+            date: '13/01/2016',
+            vehicle: 'Carro I',
+            brand: 'Marca O',
+            model: 'Modelo P',
+            color: 'Roxo',
+            totalvalue: 200000
+        },
+    ];
+
+    const dataSell = [
+        {
+            id: 1,
+            client: 'Ayrton Sandro de Bragança',
+            seller: 'José Almeida Fidalgo Lapazoli',
+            date: '18/03/2016',
+            vehicle: 'Carro X',
+            brand: 'Marca Y',
+            model: 'Modelo Z',
+            color: 'Preto',
+            initialvalue:5000,
+            installmentsvalue:1000,
+            totalvalue: 10000
+        },
+        {
+            id: 2,
+            client: 'Mario Lapazoli de Lins',
+            seller: 'José Luiz Rodrigues dos Santos',
+            date: '24/02/2016',
+            vehicle: 'Carro A',
+            brand: 'Marca Z',
+            model: 'Modelo Y',
+            color: 'Vermelho',
+            initialvalue:10000,
+            installmentsvalue:2000,
+            totalvalue: 40000
+        },
+        {
+            id: 3,
+            client: 'João Vitor Benchimol',
+            seller: 'José Almeida Fidalgo Lapazoli',
+            date: '05/02/2016',
+            vehicle: 'Carro F',
+            brand: 'Marca R',
+            model: 'Modelo V',
+            color: 'Azul',
+            initialvalue:7000,
+            installmentsvalue:500,
+            totalvalue: 10000
+        },
+        {
+            id: 4,
+            client: 'Ayrton Senna Bate Cuca',
+            seller: 'Marcos Adriano de Bragança Lins',
+            date: '13/01/2016',
+            vehicle: 'Carro I',
+            brand: 'Marca O',
+            model: 'Modelo P',
+            color: 'Roxo',
+            initialvalue:15000,
+            installmentsvalue:2500,
+            totalvalue: 200000
+        },
+    ];
+
+
+    const dataOrder = [
+        {
+            id: 1,
+            client: 'Ayrton Sandro de Bragança',
+            seller: 'José Almeida Fidalgo Lapazoli',
+            date: '18/03/2016',
+            automaker: 'Montana',
+            year: 2024,
+            color: 'Preto',
+            accessories:'Para-brisa',
+            totalvalue: 10000
+        },
+        {
+            id: 2,
+            client: 'Mario Lapazoli de Lins',
+            seller: 'José Luiz Rodrigues dos Santos',
+            date: '24/02/2016',
+            automaker: 'Endivar',
+            year: 2022,
+            color: 'Vermelho',
+            accessories:'Para-choque',
+            totalvalue: 5000
+        },
+        {
+            id: 3,
+            client: 'João Vitor Benchimol',
+            seller: 'José Almeida Fidalgo Lapazoli',
+            date: '05/02/2016',
+            automaker: 'Topgun',
+            year: 2022,
+            color: 'Preto',
+            accessories:'Motor V8',
+            totalvalue: 50000
+        },
+        {
+            id: 4,
+            client: 'Ayrton Senna Bate Cuca',
+            seller: 'Marcos Adriano de Bragança Lins',
+            date: '13/01/2016',
+            automaker: 'Ichiriro',
+            year: 2021,
+            color: 'Branco',
+            accessories:'Caixa de câmbio',
+            totalvalue: 50000
+        },
+    ];
 
 
 
@@ -52,7 +207,12 @@ const useHistory = () => {
         setYear,
         accessories, 
         setAccessories,
-        type
+        type,
+        dataBuy,
+        openItemId,
+        handlePress,
+        dataSell,
+        dataOrder
     };
 };
 
--- app/history/index.tsx
+++ app/history/index.tsx
@@ -34,10 +34,176 @@ export default function History() {
         setYear,
         accessories,
         setAccessories,
-        type
+        type,
+        dataBuy,
+        openItemId,
+        handlePress,
+        dataSell,
+        dataOrder
     } = useHistory()
 
-    const [show, setShow] = useState<boolean>(false);
+    const renderItemBuy = (item: any) => (
+        <TouchableOpacity onPress={() => handlePress(item.id)}>
+            <View
+                style={{
+                    width: 400,
+                    borderWidth: 0.6,
+                    borderColor: 'black',
+                    borderRadius: 20,
+                    padding: 20,
+                    alignSelf: 'center',
+                    marginBottom: 10,
+                    marginTop:10
+                }}
+            >
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        width: 375,
+                        alignSelf: 'center',
+                    }}
+                >
+                    <View style={{ flexDirection: 'column', height: 70, justifyContent: 'space-between' }}>
+                        <Text style={{fontWeight:'bold'}}>Compra N. {item.id}#</Text>
+                        <Text>Cliente: {item.client}</Text>
+                        <Text>Vendedor: {item.seller}</Text>
+                    </View>
+                    <View style={{ flexDirection: 'column' }}>
+                        <Text>{item.date}</Text>
+                        <MaterialIcons
+                            name={openItemId === item.id ? 'keyboard-arrow-up' : 'keyboard-arrow-down'}
+                            color="black"
+                            size={45}
+                            style={{ marginLeft: 20 }}
+                        />
+                    </View>
+                </View>
+                {openItemId === item.id && (
+                    <View style={{ width: 400, borderTopWidth: 0.6, alignSelf: 'center', marginTop: 10 }}>
+                        <View style={{ marginLeft: 14, marginTop: 10, height: 110 }}>
+                            <View style={{ flexDirection: 'column', justifyContent: 'space-between', height: 115 }}>
+                                <Text>Veículo: {item.vehicle}</Text>
+                                <Text>Marca: {item.brand}</Text>
+                                <Text>Modelo: {item.model}</Text>
+                                <Text>Cor: {item.color}</Text>
+                                <Text>Valor Total: R${item.totalvalue.toFixed(2)}</Text>
+                            </View>
+                        </View>
+                    </View>
+                )}
+            </View>
+        </TouchableOpacity>
+    );
+
+    const renderItemSell = (item: any) => (
+        <TouchableOpacity onPress={() => handlePress(item.id)}>
+            <View
+                style={{
+                    width: 400,
+                    borderWidth: 0.6,
+                    borderColor: 'black',
+                    borderRadius: 20,
+                    padding: 20,
+                    alignSelf: 'center',
+                    marginBottom: 10,
+                }}
+            >
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        width: 375,
+                        alignSelf: 'center',
+                    }}
+                >
+                    <View style={{ flexDirection: 'column', height: 70, justifyContent: 'space-between' }}>
+                        <Text style={{fontWeight:'bold'}}>Venda N. {item.id}#</Text>
+                        <Text>Cliente: {item.client}</Text>
+                        <Text>Vendedor: {item.seller}</Text>
+                    </View>
+                    <View style={{ flexDirection: 'column' }}>
+                        <Text>{item.date}</Text>
+                        <MaterialIcons
+                            name={openItemId === item.id ? 'keyboard-arrow-up' : 'keyboard-arrow-down'}
+                            color="black"
+                            size={45}
+                            style={{ marginLeft: 20 }}
+                        />
+                    </View>
+                </View>
+                {openItemId === item.id && (
+                    <View style={{ width: 400, borderTopWidth: 0.6, alignSelf: 'center', marginTop: 10 }}>
+                        <View style={{ marginLeft: 14, marginTop: 10, height: 160 }}>
+                            <View style={{ flexDirection: 'column', justifyContent: 'space-between', height: 170 }}>
+                            <Text>Veículo: {item.vehicle}</Text>
+                                <Text>Marca: {item.brand}</Text>
+                                <Text>Modelo: {item.model}</Text>
+                                <Text>Cor: {item.color}</Text>
+                                <Text>Valor Entrada: R${item.initialvalue.toFixed(2)}</Text>
+                                <Text>Valor Financiado: R${item.installmentsvalue.toFixed(2)}</Text>
+                                <Text>Valor Total: R${item.totalvalue.toFixed(2)}</Text>
+                            </View>
+                        </View>
+                    </View>
+                )}
+            </View>
+        </TouchableOpacity>
+    );
+
+
+    const renderItemOrder = (item: any) => (
+        <TouchableOpacity onPress={() => handlePress(item.id)}>
+            <View
+                style={{
+                    width: 400,
+                    borderWidth: 0.6,
+                    borderColor: 'black',
+                    borderRadius: 20,
+                    padding: 20,
+                    alignSelf: 'center',
+                    marginBottom: 10,
+                }}
+            >
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        width: 375,
+                        alignSelf: 'center',
+                    }}
+                >
+                    <View style={{ flexDirection: 'column', height: 70, justifyContent: 'space-between' }}>
+                        <Text style={{fontWeight:'bold'}}>Pedido N. {item.id}#</Text>
+                        <Text>Cliente: {item.client}</Text>
+                        <Text>Vendedor: {item.seller}</Text>
+                    </View>
+                    <View style={{ flexDirection: 'column' }}>
+                        <Text>{item.data}</Text>
+                        <MaterialIcons
+                            name={openItemId === item.id ? 'keyboard-arrow-up' : 'keyboard-arrow-down'}
+                            color="black"
+                            size={45}
+                            style={{ marginLeft: 20 }}
+                        />
+                    </View>
+                </View>
+                {openItemId === item.id && (
+                    <View style={{ width: 400, borderTopWidth: 0.6, alignSelf: 'center', marginTop: 10 }}>
+                        <View style={{ marginLeft: 14, marginTop: 10, height: 110 }}>
+                            <View style={{ flexDirection: 'column', justifyContent: 'space-between', height: 115 }}>
+                                <Text>Montadora: {item.automaker}</Text>
+                                <Text>Ano: {item.year}</Text>
+                                <Text>Cor: {item.color}</Text>
+                                <Text>Acessórios: {item.accessories}</Text>
+                                <Text>Valor Total: R${item.totalvalue.toFixed(2)}</Text>
+                            </View>
+                        </View>
+                    </View>
+                )}
+            </View>
+        </TouchableOpacity>
+    );
 
     return (
         <View style={{ flex: 1 }}>
@@ -56,32 +222,14 @@ export default function History() {
                     <Text style={{ fontSize: 20, fontWeight: '700', fontStyle: 'italic' }}>Histórico {type === 'BUY' ? 'de compra' : (type === 'SELL') ? 'de venda' : 'de pedidos'}</Text>
                 </View>
             </View>
-
-            <TouchableOpacity onPress={()=> setShow(previousState => !previousState)} >
-                <View style={{ width: 400, borderWidth: 0.6, borderColor: 'black', borderRadius: 20, padding: 20, alignSelf:'center' }}>
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: 375, alignSelf: 'center' }}>
-                        <View style={{ flexDirection: 'column', height: 70, justifyContent: 'space-between' }}>
-                            <Text>Compra N. 1#</Text>
-                            <Text>Cliente: Ayrton Sandro de Bragança</Text>
-                            <Text>Vendedor:José Almeida Fidalgo Lapazoli</Text>
-                        </View>
-                        <View style={{ flexDirection: 'column' }}>
-                            <Text>18/03/2016</Text>
-                            <MaterialIcons name={show===true?"keyboard-arrow-up":"keyboard-arrow-down"} color="black" size={45} style={{ marginLeft: 20 }} />
-                        </View>
-                    </View>
-                    {show===true &&
-                    <View style={{width:400, borderTopWidth:0.6, alignSelf:'center', marginTop:10}}>
-                        <View style={{marginLeft:14, marginTop:10}}>
-                        <Text>Veículo</Text>
-                        <Text>Marca</Text>
-                        <Text>Modelo</Text>
-                        <Text>Cor</Text>
-                        </View>
-                    </View>
-                    }
-                </View>
-            </TouchableOpacity>
+dataOrder
+            <FlatList
+                data={type==='BUY'?dataBuy:(type==='SELL')?dataSell:dataOrder}
+                renderItem={({ item }) => (type === 'BUY' ? renderItemBuy(item) : (type === 'SELL' ? renderItemSell(item) : renderItemOrder(item)))}
+                style={{ marginTop: 20 }}
+                keyExtractor={item => String(item.id)}
+                contentContainerStyle={{ paddingBottom: 20 }}
+            />
 
         </View>
     );
